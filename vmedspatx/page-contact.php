c954d79568b58d2bab196ec7a41ae5f3ff108d82
Tuyến Trần <trantrungtuyen.ictu@gmail.com>
2022-03-21 01:45:35 +0700

    Update Fields and Contact Us template

--- vmedspatx/page-contact.php
+++ vmedspatx/page-contact.php
@@ -16,12 +16,12 @@ get_header();
 ?>
 
 	<main id="primary" class="site-main">
-
+		<div class="container">
 		<?php
 		while ( have_posts() ) :
 			the_post();
 
-			get_template_part( 'template-parts/content', 'page' );
+			get_template_part( 'template-parts/content', 'page-contact-us' );
 
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
@@ -30,9 +30,9 @@ get_header();
 
 		endwhile; // End of the loop.
 		?>
+		</div>
 
 	</main><!-- #main -->
 
 <?php
-get_sidebar();
 get_footer();
