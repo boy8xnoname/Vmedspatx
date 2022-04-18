5b1d806bc4b2d67409d24853426704bd8435c0e4
Tuyến Trần <trantrungtuyen.ictu@gmail.com>
2022-04-18 09:24:19 +0700

    tyrdfgdffgd

--- medstar-01/functions.php
+++ medstar-01/functions.php
@@ -126,9 +126,8 @@ add_action( 'after_setup_theme', 'medstar01_content_width', 0 );
 function medstar01_scripts() {
 	// wp_enqueue_script( 'fontawesome', 'https://kit.fontawesome.com/54ed714a8b.js', array(), 'latest', false );
 	// wp_enqueue_style( 'fontawesome', get_template_directory_uri() . '/assets/css/all.fontawesome.css', array(), _S_VERSION);
-	wp_enqueue_style( 'aos-animate', get_template_directory_uri() . '/assets/css/aos.css', array(), _S_VERSION);
+	// wp_enqueue_style( 'aos-animate', get_template_directory_uri() . '/assets/css/aos.css', array(), _S_VERSION);
 	// wp_enqueue_style( 'elementor-icons', get_template_directory_uri() . '/assets/css/elementor-icons.css', array(), _S_VERSION);
-	// wp_enqueue_style( 'animate', get_template_directory_uri() . '/assets/css/animate.min.css', array(), _S_VERSION);
 	// wp_enqueue_style( 'style-jarallax', get_template_directory_uri() . '/assets/css/jarallax.css', array(), _S_VERSION);
 	// wp_enqueue_style( 'style-swiper', get_template_directory_uri() . '/assets/css/swiper.min.css', array(), _S_VERSION);
 	// wp_enqueue_style( 'style-magnific-popup', get_template_directory_uri() . '/assets/css/magnific-popup.css', array(), _S_VERSION);
