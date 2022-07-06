fe7ef3d51e9caa695a467a7b6706a325882c4afa
Thai <ntthai89@gmail.com>
2022-07-07 00:29:03 +0700

    update child theme

--- medstar-01-child/functions.php
+++ medstar-01-child/functions.php
@@ -1,6 +1,6 @@
 <?php
 
-add_action( 'wp_enqueue_scripts', 'medstar_child_enqueue_styles', 100);
+add_action( 'wp_enqueue_scripts', 'medstar_child_enqueue_styles');
 function medstar_child_enqueue_styles() {
 	wp_enqueue_style( 'main-style', get_template_directory_uri() . '/style.css', '' );
 }
