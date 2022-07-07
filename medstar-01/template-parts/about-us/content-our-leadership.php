45f56414124ea515119d23714e57a52fc524f2ea
Thai Nguyen <thai@beshore.com.au>
2022-07-07 16:56:33 +0700

    fix tablet header & our leader ship title

--- medstar-01/template-parts/about-us/content-our-leadership.php
+++ medstar-01/template-parts/about-us/content-our-leadership.php
@@ -1,31 +1,28 @@
 <?php if( have_rows('our_leadership') ): ?>
 <section class="about-us-content about-us-leadership">
     <div class="container">
-        <h2 class="section-title text-center">Our Exceptional Medical Leadership</h2>
-
-   
-            <div class="leadership-list">
-            <?php while( have_rows('our_leadership') ): the_row(); 
-                $member_image = get_sub_field('member_image');
-                $right_column = empty($member_image) ? 12 : 9;
-                ?>
-                <div class="leadership-item">
-                    <div class="row">
-                        <?php if(!empty( $member_image)) { ?>
-                        <div class="col col-12 col-md-3 col-xl-3">
-                            <?php echo '<img src="'.esc_url($member_image['url']).'">' ; ?>
-                        </div>
-                        <?php } ?>
-                        <div class="col col-12 col-md-<?php echo $right_column; ?> col-xl-<?php echo $right_column; ?> d-flex flex-column align-items-flex-start justify-content-center">
-                            <h3 class="leadership-name"><?php the_sub_field('member_name'); ?></h3>
-                            <h4 class="leadership-position"><?php the_sub_field('member_position'); ?></h4>
-                            <div class="leadership-content-des"><?php the_sub_field('member_description'); ?></div>
-                        </div>
-                    </div>                    
-                </div>
-            <?php endwhile; ?>
+        <h2 class="section-title text-center"><?php the_field('our_leadership_title'); ?></h2>
+        <div class="leadership-list">
+        <?php while( have_rows('our_leadership') ): the_row(); 
+            $member_image = get_sub_field('member_image');
+            $right_column = empty($member_image) ? 12 : 9;
+            ?>
+            <div class="leadership-item">
+                <div class="row">
+                    <?php if(!empty( $member_image)) { ?>
+                    <div class="col col-12 col-md-3 col-xl-3">
+                        <?php echo '<img src="'.esc_url($member_image['url']).'">' ; ?>
+                    </div>
+                    <?php } ?>
+                    <div class="col col-12 col-md-<?php echo $right_column; ?> col-xl-<?php echo $right_column; ?> d-flex flex-column align-items-flex-start justify-content-center">
+                        <h3 class="leadership-name"><?php the_sub_field('member_name'); ?></h3>
+                        <h4 class="leadership-position"><?php the_sub_field('member_position'); ?></h4>
+                        <div class="leadership-content-des"><?php the_sub_field('member_description'); ?></div>
+                    </div>
+                </div>                    
             </div>
-        
+        <?php endwhile; ?>
+        </div>
     </div>
 </section>
 
