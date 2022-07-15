e318b4c0547d821c1e41a2d89f46b1eccf05dd31
Thai Nguyen <thai@beshore.com.au>
2022-07-15 14:58:57 +0700

    update working hours on footer

--- medstar-01/template-parts/footer/footer-desktop.php
+++ medstar-01/template-parts/footer/footer-desktop.php
@@ -89,10 +89,7 @@
         </div>
         <div class="widgets third-col-widget col col-sm-4 col-md-4 col-xl-5">
             <div class="row">
-                <div class="col col-sm-4 col-lg-5 col-xl-5">
-
-                </div>
-                <div class="col col-sm-8 col-lg-7 col-xl-7">
+                <div class="col col-sm-12 col-lg-12 col-xl-12 text-end">
                     <?php
                         $timeWorking = get_theme_mod('time_working');
                         if(!empty($timeWorking)) {
