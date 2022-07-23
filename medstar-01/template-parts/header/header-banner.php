97d2b4f7539afff482c694ef35330dd9cc864484
Tuyen Tran <trantrungtuyen.ictu@gmail.com>
2022-07-23 11:19:11 +0700

    Update function for support add tablet banner for home page

--- medstar-01/template-parts/header/header-banner.php
+++ medstar-01/template-parts/header/header-banner.php
@@ -1,7 +1,8 @@
 <?php 	if ( is_front_page() || is_page_template( 'page-home.php' )) : 
 // get home page banner slider
-
+$detect = new Mobile_Detect;
 $hero_banner_style = get_field('hero_banner_style');
+
 if(have_rows('setup_banner_items')):
 ?>
 <div class="home-banner slide-<?php echo $hero_banner_style;?>">
@@ -10,33 +11,43 @@ if(have_rows('setup_banner_items')):
         <?php
         while( have_rows('setup_banner_items') ) : the_row();
             // Get parent value.
-            $banner_image = get_sub_field('banner_image');
+            $banner_image_desktop = get_sub_field('banner_image');
+            $banner_image_tablet = get_sub_field('banner_image_tablet');
             $banner_image_mobile = get_sub_field('slider_image_mobile');
             $banner_heading = get_sub_field('banner_heading');
             $banner_link = get_sub_field('banner_link');
+
+            if (!wp_is_mobile()) {
+                $banner_image = $banner_image_desktop;
+            } else {
+                if(empty($banner_image_mobile)) {
+                    $banner_image = $banner_image_desktop;
+                } else {
+                    // Any tablet device.
+                    if(!$detect->isTablet() ){
+                        // Image for mobile banner will here
+                        $banner_image = $banner_image_mobile;
+                    } else {
+                        if(empty($banner_image_tablet)) {
+                            $banner_image =  $banner_image_mobile;
+                        } else {
+                            $banner_image = $banner_image_tablet;
+                        }
+                    }
+                }
+            } 
 
             if (!empty($banner_link)) {
                 $banner_link_start = '<a href="' . $banner_link . '">';
                 $banner_link_end   = '</a>';
             }
             if($hero_banner_style == 'style1') {?> 
-                <div class="swiper-slide">
-                            <?php if(!empty($banner_image_mobile)) { ?>
-                                
-                                    <div class="card-image d-block d-sm-none" style="background-image: url('<?php echo esc_url($banner_image_mobile['url']);?>')">
-                                        <img class="d-hide d-none" src="<?php echo esc_url($banner_image_mobile['url']);?>" alt="Image Slider">
-                                    </div>
-                                
-                                    <div class="card-image d-none d-sm-block" style="background-image: url('<?php echo esc_url($banner_image['url']);?>')">
-                                        <img class="d-hide d-none" src="<?php echo esc_url($banner_image['url']);?>" alt="Image Slider">
-                                    </div>
-                                
-                            <?php } else {?>
-                                <div class="card-image" style="background-image: url('<?php echo esc_url($banner_image['url']);?>')">
-                                    <img class="d-hide d-none" src="<?php echo esc_url($banner_image['url']);?>" alt="Image Slider">
-                                </div>
-                            <?php } ?>
-                       
+                <?php if(!empty($banner_image)) {
+                    ?> 
+                    <div class="swiper-slide">
+                        <div class="card-image" style="background-image: url('<?php echo esc_url($banner_image['url']);?>')">
+                            <img class="d-hide d-none" src="<?php echo esc_url($banner_image['url']);?>" alt="Image Slider">
+                        </div>
                         <div class="card-content">
                             <?php echo apply_filters('the_content', $banner_heading);?>
                             <div class="card-action">
@@ -47,37 +58,29 @@ if(have_rows('setup_banner_items')):
                             }?> 
                             </div>
                         </div>
-                </div>
+                    </div>
+                <?php } ?>               
             <?php } else {?>
-                <div class="swiper-slide slide-<?php echo $hero_banner_style;?>">
-                        <?php if(!empty($banner_image_mobile)) { ?> 
-                                <div class="card-image d-block d-sm-none" style="background-image: url('<?php echo esc_url($banner_image_mobile['url']);?>')">
-                                    <img class="d-hide d-none" src="<?php echo esc_url($banner_image_mobile['url']);?>" alt="Image Slider">
-                                </div>
-                          
-                                <div class="card-image d-none d-sm-block" style="background-image: url('<?php echo esc_url($banner_image['url']);?>')">
-                                    <img class="d-hide d-none" src="<?php echo esc_url($banner_image['url']);?>" alt="Image Slider">
-                                </div>
-                        <?php } else {?>
-                            <div class="card-image" style="background-image: url('<?php echo esc_url($banner_image['url']);?>')">
-                                <img class="d-hide d-none" src="<?php echo esc_url($banner_image['url']);?>" alt="Image Slider">
+                <?php if(!empty($banner_image)) {?> 
+                    <div class="swiper-slide slide-<?php echo $hero_banner_style;?>">
+                        <div class="card-image" style="background-image: url('<?php echo esc_url($banner_image['url']);?>')">
+                            <img class="d-hide d-none" src="<?php echo esc_url($banner_image['url']);?>" alt="Image Slider">
+                        </div>
+                        <div class="card-content">
+                            <?php echo apply_filters('the_content', $banner_heading);?>
+                            <div class="card-action">
+                            <?php  if (!empty($banner_link)) {
+                                echo $banner_link_start;
+                                echo esc_attr('READ MORE','medstar01');
+                                echo $banner_link_end;
+                            }?> 
                             </div>
-                        <?php } ?>
-                    <div class="card-content">
-                        <?php echo apply_filters('the_content', $banner_heading);?>
-                        <div class="card-action">
-                        <?php  if (!empty($banner_link)) {
-                            echo $banner_link_start;
-                            echo esc_attr('READ MORE','medstar01');
-                            echo $banner_link_end;
-                        }?> 
                         </div>
                     </div>
-                </div>
+                <?php } ?>
             <?php } ?>
             
             <?php
-    
         endwhile;
         ?>
         </div>
@@ -92,7 +95,6 @@ if(have_rows('setup_banner_items')):
 <?php
     endif; 
 ?>
-    
 <?php else : ?>
     <div class="inner-page-banner">
     </div>
