a95cc594425f973233faaf926f7bd371014b745a
Thai <ntthai89@gmail.com>
2022-09-07 22:12:01 +0700

    update banner read more text

--- medstar-01/template-parts/header/header-banner.php
+++ medstar-01/template-parts/header/header-banner.php
@@ -53,7 +53,7 @@ if(have_rows('setup_banner_items')):
                             <div class="card-action">
                             <?php  if (!empty($banner_link)) {
                                 echo $banner_link_start;
-                                echo esc_attr('READ MORE','medstar01');
+                                echo apply_filters('medstar_read_more_text', esc_attr('Read more'));
                                 echo $banner_link_end;
                             }?> 
                             </div>
@@ -71,7 +71,7 @@ if(have_rows('setup_banner_items')):
                             <div class="card-action">
                             <?php  if (!empty($banner_link)) {
                                 echo $banner_link_start;
-                                echo esc_attr('READ MORE','medstar01');
+                                echo apply_filters('medstar_read_more_text', esc_attr('Read more'));
                                 echo $banner_link_end;
                             }?> 
                             </div>
