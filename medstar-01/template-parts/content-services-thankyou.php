0d09fca722354cbfe77c23d3d726ab04da1c04cd
Thai <ntthai89@gmail.com>
2022-11-22 22:28:13 +0700

    update faq section on thank you service

--- medstar-01/template-parts/content-services-thankyou.php
+++ medstar-01/template-parts/content-services-thankyou.php
@@ -93,4 +93,6 @@ $mobileServiceBannerImage = !empty( get_field('mobile_service_banner_image', $pa
 		</div>
 	</div><!-- .entry-content -->
 
+	<?php get_template_part( 'template-parts/services/content-faq' ); ?>
+
 </article><!-- #post-<?php the_ID(); ?> -->
--- medstar-01/template-parts/services/content-faq.php
+++ medstar-01/template-parts/services/content-faq.php
@@ -10,7 +10,7 @@
 ?>
 <section class="faq-section">
     <div class="container">
-        <h2 class="faq-title text-center h1"><?php echo strip_tags(get_field('faq_heading')); ?></h2>
+        <h2 class="faq-title text-center h1"><?php echo strip_tags(get_field('faq_heading', $parent)); ?></h2>
         <div class="accordion" id="faq_items">
             <?php 
                 while ( have_rows('faq_items', $parent) ) : the_row();
