47186d0fcbfc06ff27cc137711b72a7acd99d7a6
Thai <ntthai89@gmail.com>
2023-01-12 21:56:16 +0700

    update FAQ section

--- medstar-01/template-parts/services/content-faq.php
+++ medstar-01/template-parts/services/content-faq.php
@@ -19,7 +19,7 @@
             ?>
             <div class="accordion-item">
                 <h2 class="accordion-header" id="heading_<?php echo $id; ?>">
-                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#<?php echo $id; ?>" aria-expanded="true" aria-controls="collapseOne">
+                    <button class="accordion-button <?php echo $open_items ? '' : 'collapsed'; ?>" type="button" data-bs-toggle="collapse" data-bs-target="#<?php echo $id; ?>" aria-expanded="true" aria-controls="collapseOne">
                         <?php the_sub_field('title'); ?>
                     </button>
                 </h2>
