db54ad2ac3c544299696be10b622785fa0529200
Thai Nguyen <thai@beshore.com.au>
2023-04-20 14:02:00 +0700

    add optimize

--- medstar-01/inc/optimize.php
+++ medstar-01/inc/optimize.php
@@ -1,5 +1,7 @@
 <?php
+
 add_filter( 'script_loader_tag', function ( $tag, $handle ) {
 	if(is_admin()) return $tag;
+	if ( strpos( $tag, 'jquery' ) ) return $tag;
 	return str_replace( ' src', ' defer="defer" src', $tag );
 }, 10, 2 );
