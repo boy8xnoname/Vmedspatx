67acab73f600413aec426bfd1be5e9e15677b06b
Thai <ntthai89@gmail.com>
2023-04-21 22:49:26 +0700

    optimize

--- medstar-01/inc/optimize.php
+++ medstar-01/inc/optimize.php
@@ -2,16 +2,21 @@
 
 add_filter( 'script_loader_tag', function ( $tag, $handle ) {
 	if(is_admin()) return $tag;
+
 	if ( strpos( $tag, 'jquery' ) ) return $tag;
+
 	return str_replace( ' src', ' defer="defer" src', $tag );
 }, 10, 2 );
 
-add_action( 'wp_enqueue_scripts', 'mywptheme_child_deregister_styles', 999 );
-function mywptheme_child_deregister_styles() {
+add_action( 'wp_enqueue_scripts', 'medstar_child_deregister_styles', 999 );
+function medstar_child_deregister_styles() {
 	wp_dequeue_style( 'classic-theme-styles' );
 	wp_dequeue_style( 'gravity_forms_theme_framework' );
+	wp_deregister_style( 'gravity_forms_theme_framework' );
 	wp_dequeue_style( 'gravity_forms_orbital_theme' );
+	wp_deregister_style( 'gravity_forms_orbital_theme' );
 }
+
 add_filter( 'gform_disable_form_theme_css', '__return_true' );
 
 /**
