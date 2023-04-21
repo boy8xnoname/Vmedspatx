38072fb66e7784beed61f9151457a66966ba5ab9
Thai <ntthai89@gmail.com>
2023-04-21 18:54:02 +0700

    update optimize page

--- medstar-01/inc/optimize.php
+++ medstar-01/inc/optimize.php
@@ -5,3 +5,8 @@ add_filter( 'script_loader_tag', function ( $tag, $handle ) {
 	if ( strpos( $tag, 'jquery' ) ) return $tag;
 	return str_replace( ' src', ' defer="defer" src', $tag );
 }, 10, 2 );
+
+add_action( 'wp_enqueue_scripts', 'mywptheme_child_deregister_styles', 20 );
+function mywptheme_child_deregister_styles() {
+	wp_dequeue_style( 'classic-theme-styles' );
+}
